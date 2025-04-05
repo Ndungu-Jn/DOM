8792dd196b58eacd1bc0a93ddb8be28ad85fc268
John Ndungu <ndungulink@gmail.com>
2025-04-05 10:42:33 +0300

    Getting element by id

--- 2. Accessnig the DOM/live-coding/app.js
+++ 2. Accessnig the DOM/live-coding/app.js
@@ -1,9 +1,10 @@
 // ************ DOM SELECTORS ************
 // 1. getElementsByTagName
-console.log(document.getElementsByTagName("h1"));
-console.log(document.getElementsByTagName("h1").length);
+// console.log(document.getElementsByTagName("h1"));
+// console.log(document.getElementsByTagName("h1").length);
 
 // 2. getElementById
+console.log(document.getElementById("main"));
 // 3. getElementsByClassName
 // 4. querySelector
 // 5. querySelectorAll
