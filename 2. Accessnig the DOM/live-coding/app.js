219532ef08265ce96741660b2c7c09090071443c
John Ndungu <ndungulink@gmail.com>
2025-04-05 10:32:30 +0300

    getting Element by Tagname

--- 2. Accessnig the DOM/live-coding/app.js
+++ 2. Accessnig the DOM/live-coding/app.js
@@ -1,5 +1,8 @@
 // ************ DOM SELECTORS ************
 // 1. getElementsByTagName
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("h1").length);
+
 // 2. getElementById
 // 3. getElementsByClassName
 // 4. querySelector
