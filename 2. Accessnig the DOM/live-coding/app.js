5fad67740242b2c9ce805bee4a3a676712301ea6
John Ndungu <ndungulink@gmail.com>
2025-04-05 10:46:01 +0300

    Getting Elent by class name

--- 2. Accessnig the DOM/live-coding/app.js
+++ 2. Accessnig the DOM/live-coding/app.js
@@ -4,8 +4,9 @@
 // console.log(document.getElementsByTagName("h1").length);
 
 // 2. getElementById
-console.log(document.getElementById("main"));
+// console.log(document.getElementById("main"));
 // 3. getElementsByClassName
+console.log(document.getElementsByClassName("cls"));
 // 4. querySelector
 // 5. querySelectorAll
 
