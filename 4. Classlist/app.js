8e7f220c32f868ad464041dff6032ca9988e7145
John Ndungu <ndungulink@gmail.com>
2025-04-06 09:00:41 +0300

    Getting classlist, add and removing the class from the classlist

--- 4. Classlist/app.js
+++ 4. Classlist/app.js
@@ -1,4 +1,9 @@
 // classlist
+const h1 = document.querySelector("h1");
+// console.log(h1.classList);
 // add()
+h1.classList.add("styles");
+console.log(h1.classList);
 // remove()
+h1.classList.remove("styles");
 // toggle()
