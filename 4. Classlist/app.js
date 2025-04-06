d8220fb2f3b3af371fb93b15d0e25792d759695c
John Ndungu <ndungulink@gmail.com>
2025-04-06 09:05:43 +0300

    toggle element

--- 4. Classlist/app.js
+++ 4. Classlist/app.js
@@ -2,8 +2,10 @@
 const h1 = document.querySelector("h1");
 // console.log(h1.classList);
 // add()
-h1.classList.add("styles");
-console.log(h1.classList);
-// remove()
-h1.classList.remove("styles");
+// h1.classList.add("styles");
+// console.log(h1.classList);
+// // remove()
+// h1.classList.remove("styles");
+
 // toggle()
+h1.classList.toggle("styles"); // if there is class it removes and if there is none it adds.
