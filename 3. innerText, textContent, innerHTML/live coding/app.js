b3e35dd790d6ef899521fbdbbb9b413e7dfa0d86
John Ndungu <ndungulink@gmail.com>
2025-04-06 08:31:58 +0300

    Text content  and innerHtml

--- 3. innerText, textContent, innerHTML/live coding/app.js
+++ 3. innerText, textContent, innerHTML/live coding/app.js
@@ -3,5 +3,8 @@ const paragraph = document.querySelector("p");
 console.log(paragraph.innerText);
 
 // textContent
+const h1 = document.querySelector("h1");
+h1.innerText = "John";
 
 // innerHTML
+h1.innerHTML = "<del>Ndungu</del>";
