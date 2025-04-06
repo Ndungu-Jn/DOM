a80f160e4e5aa0c45965bb7c33ac1e81e1e937e6
John Ndungu <ndungulink@gmail.com>
2025-04-06 09:59:30 +0300

    Exercise

--- 5 Getting And Setting Attributes/exercise/app.js
+++ 5 Getting And Setting Attributes/exercise/app.js
@@ -1,4 +1,9 @@
 // 1. Select the anchor tag
+const anchor = document.querySelector("a");
+console.log(anchor);
 // 2. Use getAttribute(attrName) to check the attribute.
+console.log(anchor.getAttribute("href"));
 // 3. Select a-two & set "href" attribute
+const a2 = document.querySelector(".a-two");
+a2.setAttribute("href", "https://youtube.com/@huxnwebdev");
 // setAttribute(attrName, value)
