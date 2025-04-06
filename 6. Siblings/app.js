c8708c9d5510a44069005662d0b1f4d77c614d8c
John Ndungu <ndungulink@gmail.com>
2025-04-06 16:17:30 +0300

    Next Sbling and previous sibling

--- 6. Siblings/app.js
+++ 6. Siblings/app.js
@@ -1,13 +1,17 @@
-// parent
+// // parent
+
+// let firstLi = document.querySelector("li");
+// console.log(firstLi.parentElement);
+// console.log(firstLi.parentElement.parentElement);
+
+// // child
+
+// let ul = document.querySelector("ul");
+// console.log(ul.children);
+// console.log(ul.children[0]);
+// console.log(ul.children[1]);
+// console.log(ul.children[2]);
 
 let firstLi = document.querySelector("li");
-console.log(firstLi.parentElement);
-console.log(firstLi.parentElement.parentElement);
-
-// child
-
-let ul = document.querySelector("ul");
-console.log(ul.children);
-console.log(ul.children[0]);
-console.log(ul.children[1]);
-console.log(ul.children[2]);
+console.log(firstLi.nextElementSibling.textContent);
+console.log(firstLi.nextElementSibling.nextElementSibling.textContent);
