e5af52521ce4f0b62a839de0512e879fe748bc02
John Ndungu <ndungulink@gmail.com>
2025-04-06 16:13:54 +0300

    Accessing Parent and child

---
+++ 6. Siblings/app.js
@@ -0,0 +1,13 @@
+// parent
+
+let firstLi = document.querySelector("li");
+console.log(firstLi.parentElement);
+console.log(firstLi.parentElement.parentElement);
+
+// child
+
+let ul = document.querySelector("ul");
+console.log(ul.children);
+console.log(ul.children[0]);
+console.log(ul.children[1]);
+console.log(ul.children[2]);
