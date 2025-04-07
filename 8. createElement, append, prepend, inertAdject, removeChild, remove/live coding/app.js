a96f87ddc462b512dd8bcd339c8daaf47a28d485
John Ndungu <ndungulink@gmail.com>
2025-04-07 08:25:17 +0300

    Creating and appending to the main body

--- 8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ 8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -16,7 +16,14 @@
 // removeChild()
 // remove()
 // ------------------------------------
-const h1 = document.createElement("h1");
-h1.textContent = "Hello World";
-h1.classList.add("greetings");
-console.log(h1);
+// const h1 = document.createElement("h1");
+// const body = document.body;
+// h1.textContent = "Hello World";
+// h1.classList.add("greetings");
+// console.log(h1);
+// body.append(h1);
+const ul = document.querySelector("ul");
+
+const newLi = document.createElement("li");
+newLi.innerText = "I'm li tag";
+ul.appendChild(newLi);
