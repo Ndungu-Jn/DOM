52763930c6b93a5a4494c11753f4ac6ce3f24f22
John Ndungu <ndungulink@gmail.com>
2025-04-07 08:55:28 +0300

    The start of Event Listeners

--- 9. 3 ways to add events/app.js
+++ 9. 3 ways to add events/app.js
@@ -1,5 +1,8 @@
-// ----------- BAD WAY-----------
-
 // ----------- GREAT WAY-----------
+const best = document.querySelector(".best");
+best,
+  addEventListener("click", function () {
+    console.log("Hello wolrd");
+  });
 
 // ----------- Event (e) Object -----------
