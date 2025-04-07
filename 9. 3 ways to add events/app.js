79e262c26fbdc4efb3ad2aabc2f183f163d5fb53
John Ndungu <ndungulink@gmail.com>
2025-04-07 10:34:55 +0300

    initializing Events.

--- 9. 3 ways to add events/app.js
+++ 9. 3 ways to add events/app.js
@@ -1,16 +1,24 @@
 // ----------- GREAT WAY-----------
-const best = document.querySelector(".best");
-// best.addEventListener("click", function () {
-//   console.log("Hello wolrd");
-// });
+// const best = document.querySelector(".best");
+// // best.addEventListener("click", function () {
+// //   console.log("Hello wolrd");
+// // });
 
-// another Method -- create a function outside, then call it later.
-// function greeting() {
-//   console.log("Hello World");
-// }
-// best.addEventListener("click", greeting);
+// // another Method -- create a function outside, then call it later.
+// // function greeting() {
+// //   console.log("Hello World");
+// // }
+// // best.addEventListener("click", greeting);
 
-//arrow function way
-best.addEventListener("click", () => console.log("Yellow"));
+// //arrow function way
+// best.addEventListener("click", () => console.log("Yellow"));
 
 // ----------- Event (e) Object -----------
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log(input.value);
+});
