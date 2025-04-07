bc873144adf13eaef2932200c84cab316982a500
John Ndungu <ndungulink@gmail.com>
2025-04-07 09:00:13 +0300

    With function outside

--- 9. 3 ways to add events/app.js
+++ 9. 3 ways to add events/app.js
@@ -1,8 +1,13 @@
 // ----------- GREAT WAY-----------
 const best = document.querySelector(".best");
-best,
-  addEventListener("click", function () {
-    console.log("Hello wolrd");
-  });
+// best.addEventListener("click", function () {
+//   console.log("Hello wolrd");
+// });
+
+// another Method -- create a function outside, then call it later.
+function greeting() {
+  console.log("Hello World");
+}
+best.addEventListener("click", greeting);
 
 // ----------- Event (e) Object -----------
